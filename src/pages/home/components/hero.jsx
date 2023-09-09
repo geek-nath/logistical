62093fd8000ba503bb62acc482a60a9b115d3761
geek-nath <nath.codes@gmail.com>
2023-09-09 16:59:35 +0100

    code update

--- src/pages/home/components/hero.jsx
+++ src/pages/home/components/hero.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { socialLinks } from '../../../data/socialBtnLinks';
+import 'animate.css'
 
 const HeroBanner = () => {
   return (
     <>
       <section className='hero_banner flex items-center justify-center relative h-screen w-screen overflow-hidden'>
         <div className="hero_text px-[20px]">
-          <h1 className='text-white font-[700] text-[2rem] md:text-[4rem] lg:text-[4.5rem] leading-[1]'>Logistics Excellence: <br /> Beyound Boundries</h1>
-          <p className="mt-[50px] w-full md:w-[70%] text-white">
+          <h1 className='text-white animate_animated animate__fadeIn font-[700] text-[40px] md:text-[60px] xl:text-[76px] xl:leading-[76px] leading-[40px] md:leading-[60px]'>Logistics Excellence: <br /> Beyond Boundries</h1>
+          <p className="mt-[50px] w-full md:w-[70%] text-white lg:leading-[31px] lg:text-[18px]">
             Optimized logistics can reduce transportation, storage, and inventory costs, leading to higher profitability.
           </p>
         </div>
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import HeroBanner from './components/hero'
 import './assets/sass/home.scss'
 import LogisticsCapabilities from './components/logisticsCapabilities'
+import FeaturesSlider from './components/featuresSlider'
+import ServiceSteps from './components/serviceSteps'
 
 const HomePage = () => {
   return (
@@ -9,6 +11,8 @@ const HomePage = () => {
       <main>
         <HeroBanner />
         <LogisticsCapabilities />
+        <FeaturesSlider />
+        <ServiceSteps />
       </main>
     </>
   )
