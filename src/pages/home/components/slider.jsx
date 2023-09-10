f4aeb868ebe83aedd10b2c67fd8f6866ee648aa5
geek-nath <nath.codes@gmail.com>
2023-09-10 21:45:36 +0100

    project init - code update

--- src/pages/home/components/slider.jsx
+++ src/pages/home/components/slider.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
-import 'swiper/css/effect-coverflow'
 import 'swiper/css/navigation'
 import { imageSliderData } from '../../../data/carouselSliderImage'
+import { Navigation } from 'swiper/modules'
 import * as AiIcons from 'react-icons/ai'
 
 const CarouselSlider = () => {
@@ -11,14 +11,7 @@ const CarouselSlider = () => {
     <>
       <section className='py-[5rem] overflow-hidden bg-white'>
         <div className='px-[20px] lg:px-[50px] w-full'>
-          <Swiper effect='coverflow' grabCursor={true} centeredSlides={true} loop={true} slidesPerView={"auto"} coverflowEffect={
-            {
-              rotate: 0,
-              stretch: 0,
-              depth: 100,
-              modifier: 2.5
-            }
-          } className='flex justify-center'>
+          <Swiper data-aos="fade-left" data-aos-duration="3000" grabCursor={true} centeredSlides={true} loop={true} slidesPerView={"auto"} modules={[Navigation]} navigation={{ nextEl: ".next_btn", prevEl: ".prev_btn", clickable: true }} effect='coverflow' coverflowEffect={{ rotate: 0, stretch: 0, depth: 100, modifier: 2.5 }} className='flex justify-center'>
             {imageSliderData.map((sliderData, index) => {
               const { imageUrl, description } = sliderData;
               return (
@@ -29,14 +22,14 @@ const CarouselSlider = () => {
                 </SwiperSlide>
               )
             })}
-            {/* <div className="flex items-center justify-center gap-4 mt-[30px]">
+            <div className="flex items-center justify-center gap-4 mt-[30px]">
               <button className='h-[50px] prev_btn w-[50px] text-gray-800 rounded-[50%] border border-gray-800 flex items-center justify-center'>
                 <AiIcons.AiOutlineArrowLeft />
               </button>
               <button className='h-[50px] w-[50px] next_btn text-gray-800 rounded-[50%] border border-gray-800 flex items-center justify-center'>
                 <AiIcons.AiOutlineArrowRight />
               </button>
-            </div> */}
+            </div>
           </Swiper>
         </div>
       </section>
