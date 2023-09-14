ddce7c6b924df82be9213775fdddd8e029f36540
geek-nath <nath.codes@gmail.com>
2023-09-14 19:43:55 +0100

    code update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/home";
 import './assets/sass/global.scss'
-import Navbar from "./components/navbar";
 import PageNotFound from "./pages/page_not_found/pageNotFound";
 import Aos from "aos";
 import 'aos/dist/aos.css'
